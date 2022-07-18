421a6a5df47e2510226a935897cf674b4f2a0e52
George Olufemi <georgeoluolufemi@gmail.com>
2022-07-18 14:11:57 +0100

    Finished Description Section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Bicycle from './components/Bicycle';
 //import Heroi from './components/Heroi';
 import Heroii from './components/Heroii';
 import Slider from './components/Slider';
@@ -9,6 +10,40 @@ function App() {
       {/* <Heroi /> */}
       <Heroii />
       <Slider />
+      <div className="md:flex">
+        <Bicycle 
+          bg="bg-[#2A0072]"
+          title="Tee Bike"
+          price="$100 - $200"
+          spec1="Cheap"
+          spec2="Antique"
+          spec3="Not Egronomic"
+        />
+        <Bicycle 
+          bg="bg-[#670DFF]"
+          title="Gear Bike"
+          price="$250 - $400"
+          spec1="Cool"
+          spec2="Speed"
+          spec3="Egronomic"
+        />
+        <Bicycle 
+          bg="bg-[#11012C]"
+          title="Hiking Bike #001"
+          price="$400 - $500"
+          spec1="Not Classy"
+          spec2="Unique"
+          spec3="Rocky"
+        />
+        <Bicycle 
+          bg="bg-[#2A0072]"
+          title="Sports Bike"
+          price="$500 - $900"
+          spec1="Elegant"
+          spec2="Speed"
+          spec3="Egronomic"
+        />
+      </div>
     </>
   );
 }
--- src/components/Bicycle.jsx
+++ src/components/Bicycle.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
-export default function Bicycle() {
-  return <div>Bicycle</div>;
+export default function Bicycle(props) {
+  return(
+    <>
+      <div className={`h-[306px] md:w-[362px] ${props.bg} text-center p-10 text-white`}>
+        <h1 className="font-[700] text-[28px] leading-[33px] mb-[20px]">{ props.title  }</h1>
+        <p className="font-[600] text-[28px] leading-[33px] text-[#E4D5FF] mb-[16px]">{ props.price  }</p>
+        <p className="font-[400] text-[28px] leading-[33px] text-[#E4D5FF] mb-[10px]">{ props.spec1}</p>
+        <p className="font-[400] text-[28px] leading-[33px] text-[#E4D5FF] mb-[10px]">{ props.spec2  }</p>
+        <p className="font-[400] text-[28px] leading-[33px] text-[#E4D5FF] mb-[10px]">{ props.spec3  }</p>
+      </div>
+    </>
+  );
 }
