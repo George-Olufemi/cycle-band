416c0aa80b254364719396cd5a9881244b469420
George Olufemi <georgeoluolufemi@gmail.com>
2022-07-18 10:11:31 +0100

    Installed TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div className="flex justify-center text-center">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="/logo512.png" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="underline text-4xl"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
