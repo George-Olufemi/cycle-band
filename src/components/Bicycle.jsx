c4bf1067086ab778b4346c8b57365e06d93dd559
George Olufemi <georgeoluolufemi@gmail.com>
2022-07-21 13:01:32 +0100

    Refactor

--- src/components/Bicycle.jsx
+++ src/components/Bicycle.jsx
@@ -5,10 +5,10 @@ export default function Bicycle(props) {
     <>
       <div className={`h-[306px] md:w-[362px] ${props.bg} text-center p-10 text-white`}>
         <h1 className="font-[700] text-[28px] leading-[33px] mb-[20px]">{ props.title  }</h1>
-        <p className="font-[600] text-[28px] leading-[33px] text-[#E4D5FF] mb-[16px]">{ props.price  }</p>
-        <p className="font-[400] text-[28px] leading-[33px] text-[#E4D5FF] mb-[10px]">{ props.spec1}</p>
-        <p className="font-[400] text-[28px] leading-[33px] text-[#E4D5FF] mb-[10px]">{ props.spec2  }</p>
-        <p className="font-[400] text-[28px] leading-[33px] text-[#E4D5FF] mb-[10px]">{ props.spec3  }</p>
+        <p className="llnp">{ props.price  }</p>
+        <p className="llnp">{ props.spec1}</p>
+        <p className="llnp">{ props.spec2  }</p>
+        <p className="llnp">{ props.spec3  }</p>
       </div>
     </>
   );
